711feb5f957bc58e5ae0d9d20cf1a172785e5726
Đặng Hoàng Dương <108934396+duongbncf@users.noreply.github.com>
2023-05-09 02:29:14 +0700

    lan5

--- testGit - Copy.cpp
+++ testGit - Copy.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-	cout << "Hello world ! 233333333";
-=======
-	cout << "Hello world ! 2222222222";
->>>>>>> feada3a7b10c65d1e1b48a63c922c972a7329449
+	
 	cout << "Hello world ! 4";
 	return 0;
 }
--- testGit.cpp
+++ testGit.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "Hello world ! 2";
+	cout << "Hello world ! 3";
 	return 0;
 }
